72098b522dbd01088e5ab6d7e9805e912ac4bd64
= <kaven.feng@autodesk.com>
2015-09-20 18:07:53 +0800

    render a movie

--- reactnative/index.ios.js
+++ reactnative/index.ios.js
@@ -4,29 +4,33 @@
  */
 'use strict';
 
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
 
 var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } = React;
 
 var reactnative = React.createClass({
   render: function() {
+
+    var movie = MOCKED_MOVIES_DATA[0];
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Image source={{uri:movie.posters.thumbnail}}
+              style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
     );
   }
@@ -35,19 +39,25 @@ var reactnative = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  rightContainer: {
+    flex: 1,
   },
-  instructions: {
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail:{
+    width: 53,
+    height: 81
   },
 });
 
